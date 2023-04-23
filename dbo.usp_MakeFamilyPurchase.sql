59d8f1790bbf9f97df9b68d4ef4b03de98499b44
A.Sultanbekov <axa.sultan@mail.ru>
2023-04-23 17:14:44 +0300

    dbo.usp_MakeFamilyPurchase в ветке Procedures

---
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -0,0 +1,22 @@
+DROP PROC IF EXISTS dbo.usp_MakeFamilyPurchase
+GO
+
+CREATE PROC dbo.usp_MakeFamilyPurchase
+    @FamilySurName nvarchar(255)
+    AS
+IF @FamilySurName in (SELECT SurName FROM dbo.Family)
+    WITH C AS (
+        SELECT SUM(VALUE) AS val
+        FROM dbo.Basket
+        WHERE ID_Family = (
+            SELECT ID
+            FROM dbo.Family
+            WHERE SurName = @FamilySurName)
+        GROUP BY ID_Family
+        )
+    UPDATE dbo.Family
+    SET BudgetValue -= (SELECT * FROM C)
+    WHERE SurName = @FamilySurName;
+ELSE
+    PRINT @FamilySurName + N' - такой семьи нет';
+GO
