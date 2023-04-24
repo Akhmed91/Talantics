1f15a2725f0d487df4076b444b61048d99696829
A.Sultanbekov <axa.sultan@mail.ru>
2023-04-24 12:51:52 +0300

    add begin

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -5,18 +5,22 @@ CREATE PROC dbo.usp_MakeFamilyPurchase
     @FamilySurName nvarchar(255)
     AS
 IF @FamilySurName in (SELECT SurName FROM dbo.Family)
-    WITH C AS (
-        SELECT SUM(VALUE) AS val
-        FROM dbo.Basket
-        WHERE ID_Family = (
-            SELECT ID
-            FROM dbo.Family
-            WHERE SurName = @FamilySurName)
-        GROUP BY ID_Family
-        )
-    UPDATE dbo.Family
-    SET BudgetValue -= (SELECT * FROM C)
-    WHERE SurName = @FamilySurName;
+    BEGIN
+        WITH C AS (
+            SELECT SUM(VALUE) AS val
+            FROM dbo.Basket
+            WHERE ID_Family = (
+                SELECT ID
+                FROM dbo.Family
+                WHERE SurName = @FamilySurName)
+            GROUP BY ID_Family
+            )
+        UPDATE dbo.Family
+        SET BudgetValue -= (SELECT * FROM C)
+        WHERE SurName = @FamilySurName;
+    END;
 ELSE
-    PRINT @FamilySurName + N' - такой семьи нет';
+    BEGIN
+        PRINT @FamilySurName + N' - такой семьи нет';
+    END
 GO
